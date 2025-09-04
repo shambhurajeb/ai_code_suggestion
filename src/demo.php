5675c2d33901b4c212a589dd82d398c0aa892b93
shambhurajeb <shambhurajeb@alohatechnologydev.com>
2025-09-04 18:21:04 +0530

    Update demo.php

--- src/demo.php
+++ src/demo.php
@@ -1,1 +1,25 @@
 
+<?php
+/**
+ * Demo file for GitHub Actions workflow.
+ *
+ * @package DemoProject
+ */
+
+/**
+ * Demo function to greet a user.
+ *
+ * @param string $name Name of the user.
+ *
+ * @return void
+ */
+function demoFunction( string $name ): void
+{
+    echo 'Hello ' . $name;
+}
+
+// Call the demo function
+demoFunction( 'GitHub Actions' );
+
+// Remove unused variable to pass PHPStan as well
+// $unusedVar = 123; // unused
